552436bf9926863cd856f35dc37b0b9b3074d336
hustluy <hustluy@gmail.com>
2013-05-09 20:45:33 +0800

    初步完成爬虫任务---可以进行csdn上的博客文章下载---但是文章中的html实体符号解析还存在着问题

--- src/com/spider/HtmlParserTool.java
+++ src/com/spider/HtmlParserTool.java
@@ -1,7 +1,15 @@
 package com.spider;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -10,27 +18,66 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.htmlparser.Node;
 import org.htmlparser.NodeFilter;
 import org.htmlparser.Parser;
+import org.htmlparser.beans.StringBean;
+import org.htmlparser.filters.NodeClassFilter;
+import org.htmlparser.nodes.TextNode;
+import org.htmlparser.tags.Div;
 import org.htmlparser.tags.LinkTag;
+import org.htmlparser.tags.ParagraphTag;
+import org.htmlparser.util.NodeIterator;
 import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
 
 public class HtmlParserTool
 {
-
 	/**
 	 * @param args
 	 */
 	private  Parser parser =  null;
-	
+	public static String[] entities = {"&nbsp;", "&quot;", "&apos;", "&amp;", "&lt;", "&gt;"};
+	public static String[] tranfers = {" ", "\"", "'", "&", "<", ">"};
+	public static String replaceAll(String content)
+	{
+		StringBuilder result = new StringBuilder();
+		for(int i = 0; i < content.length(); ++i)
+		{
+			char ch = content.charAt(i);
+			if(ch == '&')
+			{
+				String temp = null;
+				int j;
+				for(j = 0; j < entities.length; ++j)
+				{
+					int beg = i;
+					int end = Math.min(beg + entities[j].length(), content.length());
+					temp = content.substring(beg, end);
+					//System.out.println("temp = " + temp);
+					if( entities[j].equals(temp) )
+					{
+						result.append(tranfers[j]);
+						i = end - 1;
+						break;
+					}
+				}
+				if(j >= entities.length)
+					result.append(ch);
+			}
+			else result.append(ch);
+		}
+		return result.toString();
+	}
 	public void test()
 	{
 		try
 		{
 			HttpClient httpClient = new DefaultHttpClient();
-			HttpGet httpGet = new HttpGet("http://blog.csdn.net");
+			HttpGet httpGet = new HttpGet("http://blog.csdn.net/hustluy/article/details/8829159");
+			
+			
 			String content = httpClient.execute(httpGet, new BasicResponseHandler());
-			System.out.println(content);
-			parser = new Parser(content);
+			//System.out.println(content);
+			parser = Parser.createParser(content, "ISO8859-1");
+			
 			NodeFilter nodeFilter = new NodeFilter()
 			{
 
@@ -38,25 +85,29 @@ public class HtmlParserTool
 				public boolean accept(Node node)
 				{
 					// TODO Auto-generated method stub
-					if(node instanceof LinkTag)
+					if(node instanceof Div)
 					{
-						//System.out.println(((LinkTag) node).getLink());
-						//if(((LinkTag) node).getLink().endsWith("html"))
+						String attribute = ((Div)node).getAttribute("id");
+						if(attribute != null && attribute.equalsIgnoreCase("article_content"))
 							return true;
+						else return false;
 					}
-					return false;
+					else return false;
 				}
 				
 			};
+
+			
 			NodeList nodeList = parser.extractAllNodesThatMatch(nodeFilter);
 			for(int i = 0; i < nodeList.size(); ++i)
 			{
-				//System.out.println(((LinkTag)nodeList.elementAt(i)).getLink());
+				Node node = nodeList.elementAt(i);
+				String articleContent = this.replaceAll(node.toPlainTextString());
+				System.out.println(articleContent);
 			}
 			
 		} catch (ParserException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (ClientProtocolException e)
 		{
@@ -69,10 +120,57 @@ public class HtmlParserTool
 		} 
 	}
 	
-	public static void main(String[] args)
+	public static void scanRootURL(String url) 
+			throws ClientProtocolException, IOException, ParserException, URISyntaxException
+	{
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(url);
+		BasicResponseHandler responseHandler = new BasicResponseHandler();
+		String content = httpClient.execute(httpGet, responseHandler);
+		Parser parser = new Parser(content);
+		NodeFilter nodeFilter = new NodeFilter()
+		{
+
+			@Override
+			public boolean accept(Node node)
+			{
+				// TODO Auto-generated method stub
+				if(node instanceof LinkTag)
+				{
+					return true;
+				}
+				return false;
+			}
+			
+		};
+		NodeList nodeList = parser.extractAllNodesThatMatch(nodeFilter);
+		for(int i = 0; i < nodeList.size(); ++i)
+		{
+			//System.out.println();
+			//httpGet.abort();
+			String tempUrl = ((LinkTag)nodeList.elementAt(i)).getLink();
+			if(tempUrl.indexOf("http://") == -1)
+				tempUrl = "http://blog.csdn.net" + tempUrl;
+			try
+			{
+				httpGet.setURI(new URI(tempUrl));
+			
+			String temp = httpClient.execute(httpGet, responseHandler);
+			//System.out.println(temp);
+			}
+			catch(Exception e)
+			{
+				System.out.println(tempUrl);
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	public static void main(String[] args) throws Exception
 	{
 		// TODO Auto-generated method stub
 		new HtmlParserTool().test();
+		//HtmlParserTool.scanRootURL("http://blog.csdn.net");
 	}
 
 }
--- src/com/spider/MyCrawler.java
+++ src/com/spider/MyCrawler.java
@@ -1,17 +1,66 @@
 package com.spider;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HeaderElementIterator;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.HttpVersion;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.BufferedHttpEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicHeaderElementIterator;
+import org.apache.http.message.BasicHttpRequest;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+import org.htmlparser.Node;
+import org.htmlparser.NodeFilter;
+import org.htmlparser.Parser;
+import org.htmlparser.filters.LinkRegexFilter;
+import org.htmlparser.filters.OrFilter;
+import org.htmlparser.tags.Div;
+import org.htmlparser.tags.LinkTag;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
 
 
 public class MyCrawler
@@ -19,64 +68,210 @@ public class MyCrawler
 	private static String rootUrl = "http://blog.csdn.net";
 	private static String prefix = "http://blog.csdn.net";
 	private Queue<String> queue = new LinkedList<String>();
-	private  HashSet<String> visitedUrls = new HashSet<String>();
+	private  HashSet<String> visitedURLs = new HashSet<String>();
+	private Pattern articleURLPattern = Pattern.compile(this.prefix 
+			+ "(/\\w+/article/details/\\d+)$");
+	private static int count = 0;
+	private HttpClient httpClient = null;
+	private static File parentDir = new File("data");
 	
-	public String fetchFileName(String url)
+	public void beginCrawling(String seedURL)
 	{
-		int beg = url.lastIndexOf('/') + 1;
-		return url.substring(beg);
+		this.httpClient = new DefaultHttpClient();
+		this.queue.add(seedURL);
+		while(!this.queue.isEmpty() && count <= 1000)
+		{
+			String url = this.queue.poll();
+			String html = null;
+			try
+			{
+				html = this.downloadPageFrom(url);
+			}
+			catch(Exception e)
+			{
+				HttpResponseException he = (HttpResponseException)e;
+				System.out.println(he.getMessage() + "-----" + "program will sleep!");
+				if(he.getStatusCode() == HttpStatus.SC_FORBIDDEN)
+				{
+					try
+					{
+						Thread.sleep(1000000);
+						html = this.downloadPageFrom(url);
+					} catch (InterruptedException e1)
+					{
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+			this.retrieveURLs(html);
+			if( articleURLPattern.matcher(url).find() )
+			{
+				System.out.println(url + "is an article!"); 
+				this.extractArticleFromPage(url, html);
+			}
+			else System.out.println(url + " is not an article!");
+		}
 	}
-	public void downloadPageFrom(String url)
+	
+	public String downloadPageFrom(String url)
 	{
+		HttpGet httpGet = new HttpGet(url);
+		ResponseHandler<String> responseHandler = new BasicResponseHandler();
+		String html = null;
 		try
 		{
-			String fileName = fetchFileName(url);
-			System.out.println(fileName);
-			FileOutputStream fos = new FileOutputStream(fileName);
-			OutputStreamWriter osw = new OutputStreamWriter(fos);
-			BufferedWriter bw = new BufferedWriter(osw);
+			html = httpClient.execute(httpGet, responseHandler);
+			//this.retrieveUrl(html);
 			
-			HttpClient httpClient = new DefaultHttpClient();
-			HttpGet httpGet = new HttpGet(url);
-			ResponseHandler<String> responseHandler = new BasicResponseHandler(); 
-			String content = httpClient.execute(httpGet, responseHandler);
-			bw.write(content);
-			fos.close();
-			this.retrieveUrl(content);
-			//System.out.println(content);
-			
-		}
-		catch(Exception e)
+		} catch (ClientProtocolException e)
 		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally
+		{
+			httpGet.abort();
+		}
+		return html;
+	}
+	
+	public void retrieveURLs(String html)
+	{
+		// retrivel url like http://blog.csdn.net/username(/article/details/id) 
+		//                         or /username/article/details/id
+		try
+		{
+			Parser htmlParser = new Parser(html);
+			
+			LinkRegexFilter outLinkFilter = new LinkRegexFilter(this.prefix 
+					+ "/\\w+(/article/details/\\d+)?$");
+			LinkRegexFilter inLinkFilter = new LinkRegexFilter("(/\\w+/article/details/\\d+)$");
+			OrFilter orLinkFilter = new OrFilter(inLinkFilter, outLinkFilter);
+			
+			NodeList nodeList = htmlParser.parse(orLinkFilter);
+			for(int i = 0; i < nodeList.size(); ++i)
+			{
+				LinkTag linkTag = (LinkTag)nodeList.elementAt(i);
+				String link = linkTag.getLink();
+				if( link.indexOf(this.prefix) == -1)
+					link = this.prefix + link;
+				if( !visitedURLs.contains(link) )
+				{
+					//System.out.println(link);
+					queue.add(link);
+					visitedURLs.add(link);
+				}
+			}
+		} catch (ParserException e)
+		{
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public String fetchFilepath(String url)
+	{
+		int beg = this.prefix.length() + 1;
+		int end = url.indexOf('/', beg);
+		if( !this.parentDir.exists() )
+			this.parentDir.mkdir();
+		String dirname = this.parentDir.toString() + File.separator + url.substring(beg, end);
+		File dir = new File(dirname);
+		if( !dir.exists() )
+		{
+			if( !dir.mkdir() )
+			{
+				System.out.println("Fails to create " + dirname);
+				System.exit(1);
+			}
+		}
+		String filename = url.substring(url.lastIndexOf('/')+1);
+		return  dirname + File.separator + filename;
+	}
+	
+	public void extractArticleFromPage(String url, String html)
+	{
+		String filepath = fetchFilepath(url);
+		Parser htmlParser = null;
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		try
+		{	
+			fw = new FileWriter(filepath);
+			bw = new BufferedWriter(fw);
+			htmlParser = new Parser(html);
+			NodeFilter articleFilter = new NodeFilter()
+			{
+				@Override
+				public boolean accept(Node node)
+				{
+					// TODO Auto-generated method stub
+					if( (node instanceof Div) )
+					{
+						Div div = (Div)node;
+						if( div.getAttribute("id") != null 
+								&& div.getAttribute("id").equalsIgnoreCase("article_content")
+								&& div.getAttribute("class") != null
+								&& div.getAttribute("class").equalsIgnoreCase("article_content") )
+							return true;
+						else return false;
+					}
+					return false;
+				}
+			};
+			NodeList nodeList = htmlParser
+					.extractAllNodesThatMatch(articleFilter);
+			for (int i = 0; i < nodeList.size(); ++i)
+			{
+				Node node = nodeList.elementAt(i);
+				String partContent = HtmlParserTool.replaceAll(node
+						.toPlainTextString());
+				bw.write(partContent);
+			}
+			System.out.println(filepath + "has been loaded!");
+			bw.close();
+			fw.close();
+			
+		} catch (ParserException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 		
 	}
 	
-	public void retrieveUrl(String content)
-	{
-		
-		/*Pattern pattern = Pattern.compile("<a href=\"/hejingyuan6/article/details/\\d+\">");
-		Matcher matcher = pattern.matcher(content);
-		while(matcher.find())
-		{
-			String matcherResult = matcher.group();
-			System.out.println(matcherResult);
-			String suffix = 
-					matcherResult.substring(matcherResult.indexOf('/'), matcherResult.lastIndexOf('\"'));
-			String url = MyCrawler.prefix + suffix;
-			if( !visitedUrls.contains(url) )
-			{
-				visitedUrls.add(url);
-				this.downloadPageFrom(url);
-			}
-		}*/
-	}
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		new MyCrawler().downloadPageFrom(MyCrawler.rootUrl);
-		System.out.println("download finished!");
+		//new MyCrawler().downloadPageFrom("http://blog.csdn.net/hustluy/article/details/8482430");
+		
+		//Generate a dir to store data crawled
+		
+		new MyCrawler().beginCrawling(MyCrawler.rootUrl);
+		System.out.println("Game Over!");
 	}
 
 }
+
+/*Pattern pattern = Pattern.compile("<a href=\"/hejingyuan6/article/details/\\d+\">");
+Matcher matcher = pattern.matcher(content);
+while(matcher.find())
+{
+	String matcherResult = matcher.group();
+	System.out.println(matcherResult);
+	String suffix = 
+			matcherResult.substring(matcherResult.indexOf('/'), matcherResult.lastIndexOf('\"'));
+	String url = MyCrawler.prefix + suffix;
+	if( !visitedUrls.contains(url) )
+	{
+		visitedUrls.add(url);
+		this.downloadPageFrom(url);
+	}
+}*/
